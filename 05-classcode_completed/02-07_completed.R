69d18fa28da3e5d74922bf5d276a0e3adbd4d53d
Leonardo Bastos <lmbastos@Leonardos-MBP.lan>
2022-02-09 21:56:15 -0500

    added Feb 07 completed script

--- 05-classcode_completed/02-07_completed.R
+++ 05-classcode_completed/02-07_completed.R
@@ -1,7 +1,6 @@
 ## 2022 CRSS 4060/6060 - Feb 07 Agenda
 
 # Housekeeping:
-## Check if downloaded latest version of GitHub
 ## Blog: instructions (bookmark it!)
 ## GitHub: material, constantly being updated  
 
@@ -27,7 +26,6 @@
 ### Colored syntax
 ### Syntax suggestions
 
-
 ## RStudio panels
 ### Script  
 ### Console
@@ -36,12 +34,12 @@
 
 # 2) R terminology ----
 ## Object
-43/13
-
-a <- 43/13
+45
+45/13
+a <- 45/13
 a
 
-b <- c(10, 5, 2)
+b <- c(10,15, 5)
 b
 
 c <- "workshop"
@@ -54,83 +52,79 @@ d <- data.frame(number = b,
 
 d
 
-b/13
-
 ### Matrices
-
-e <- matrix(c(b,b), ncol = 2 )
+e <- matrix(c(b,b), 
+            ncol = 2 )
 e
 
-# concatenate 
-
 ### Lists 
-f <- list("number" = a,
-          "numbers" = b,
-          "data" = d
-          )
+f <- list("number" = a, "numbers" = b,
+          "word" = c, "data" = d)
 
 f
 
 class(f)
 
-class(f$data)
+class(f$number)
 
-class(d$id)
 
 ## Function
 mean(b)
 
+mean(x = b)
 
 ## Argument
 help("mean")
-b
 
-b2 <- c(10,5,2,NA)
-b2
+b2 <- c(10,15,5,NA)
 
-mean(b2, na.rm = TRUE)
+mean(b2)
 
-mean(b2, TRUE)
+mean(x = b2, 
+     na.rm = TRUE)
 
-mean(na.rm = TRUE, x = b2)
+mean(b2, 
+     TRUE)
 
-help("mean")
+mean(TRUE,
+     b2)
 
+mean(na.rm = TRUE, 
+     x = b2)
 
 ## Package
 ## Install vs. load a package
 ## Let's install package tibble, then load it
 install.packages("tibble")
-
 library(tibble)
 
 # 3) Creating a data set, exploring it ----
 intro <- tribble(~name, ~height, ~pineapple, 
                  "Leo", 1.80, T,
-                 "George", 1.78, F,
-                 "Emily", 1.65, F,
-                 "Victoria", 1.62, T
+                 "Leo2", 1.83, F,
+                 "Leo3", 1.82, F,
+                 "Leo4", 1.81, T,
                  )
-
-intro
   
 # Check class, summary, and structure
 class(intro)
-
 summary(intro)
 
 head(intro, n = 2)
 tail(intro, n = 2)
 
+class(intro$pineapple)
+
 # Sampling the dataset
 # First row only
-intro[ 1, ]
+intro[1 , ]
+
 
 # First column only
 intro[ , 1]
 
 # Rows 1 to 3 and columns 1 to 3
-intro[1:3, 1:3 ]
+intro[1:3 , 1:3]
 
 # Rows 1 and 3 and columns 1 and 3
 intro[c(1,3) , c(1,3)]
@@ -138,27 +132,21 @@ intro[c(1,3) , c(1,3)]
 # 4) ggplot2 philosophy and plots ---- 
 
 library(ggplot2)
-
-ggplot(data = intro, mapping = aes(x = height,
-                                   y = pineapple))+
-  #geom_point(color="red", shape = 2)
-  geom_point(aes(shape=name, color=name))
-
-ggsave("~/Desktop/plot1.png")
-
 # Point
-intro
+ggplot(data = intro, 
+       mapping = aes(y = height, 
+                     x = name, 
+                     color = pineapple))+
+  geom_point()
 
 # Exporting
-
-x = height
-y = pineaple 
+ggsave("~/Desktop/plot1.png")
 
 # 5) RStudio projects ----
 ## Create a folder on your Desktop named 2022_AdvPA-initials
 ## Create sub-folders data, code, output
 ## Create an RStudio project at the level of main folder (e.g., 2022_AdvPA-initials)
-## Create an Rmardkwon file (just to explore, not saving it) 
+## Create an Rmarkdwon file (just to explore, not saving it) 
 
 # 6) Assignment #1 (due on Tuesday Feb 8th 11:59 pm)  
 
